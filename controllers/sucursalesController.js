50743297d7b4b4a9d8447ef8a0cf77eb7eb90163
Belen Medina Roldan <bmedinaroldan@gmail.com>
2020-11-30 00:51:47 -0300

    Se valida la sucursal ingresada

--- controllers/sucursalesController.js
+++ controllers/sucursalesController.js
@@ -13,17 +13,23 @@ module.exports = {
     },
     getBranch: (req,res) => {
         let branch = carDealerships.find(carDealership => carDealership.sucursal.toLowerCase() == req.params.sucursal.toLowerCase() );
+        //console.log(branch)
         res.set({'content-type':'text/plain;charset=utf-8'});
-        res.write(`${branch.sucursal} \n`);
-        res.write(`\t - Direccion: ${branch.direccion} \n`);
-        res.write(`\t - Telefono: ${branch.telefono} \n\n`);
-        res.write('\t Nuestros autos:\n\n');
-        branch.autos.forEach(element => {
-            res.write(`\t\t Marca: ${element.marca} \n`);
-            res.write(`\t\t Modelo: ${element.modelo} \n`);
-            res.write(`\t\t Año: ${element.anio} \n`);
-            res.write(`\t\t Color: ${element.color} \n\n`);
-        });
+
+        if (branch === undefined) {
+            res.write(`La sucursal ${req.params.sucursal.toUpperCase()} no existe`);
+        } else {
+            res.write(`${branch.sucursal} \n`);
+            res.write(`\t - Direccion: ${branch.direccion} \n`);
+            res.write(`\t - Telefono: ${branch.telefono} \n\n`);
+            res.write('\t Nuestros autos:\n\n');
+            branch.autos.forEach(element => {
+                res.write(`\t\t Marca: ${element.marca} \n`);
+                res.write(`\t\t Modelo: ${element.modelo} \n`);
+                res.write(`\t\t Año: ${element.anio} \n`);
+                res.write(`\t\t Color: ${element.color} \n\n`);
+            });            
+        }
         res.end()
     }
 }
